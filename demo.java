05abb25098989ff1a5575454fe87d6a2376a45a7
Poojetha <dkpooji247@gmail.com>
2025-09-16 15:35:09 +0530

    functions with parameter

--- demo.java
+++ demo.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class demo {
-    int apple_price= 20;
-    int apple_count = 10;
+    int apple_price;
+    int apple_count ;
    
-    void total_money(){
+    void total_money(int apple_price,int apple_count){
         int total = apple_price*apple_count;
         System.out.println(total);
     }
     public static void main(String args[]) {
        demo d1 = new demo();
-       d1.total_money();
+       d1.total_money(3,9);
         }
     }
 
