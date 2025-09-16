6c3e1780f75efa51911ffc145fc37bc0df064120
Poojetha <dkpooji247@gmail.com>
2025-09-16 15:31:59 +0530

    function

--- demo.java
+++ demo.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class demo {
-    int coffee = 20;
-    int tea = 10;
-    int juice = 15;
-    void sum(){
-        int total = coffee +tea+juice;
+    int apple_price= 20;
+    int apple_count = 10;
+   
+    void total_money(){
+        int total = apple_price*apple_count;
         System.out.println(total);
     }
     public static void main(String args[]) {
        demo d1 = new demo();
-       d1.sum();
+       d1.total_money();
         }
     }
 
