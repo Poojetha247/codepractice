f9dbeaace0273b974170a6f49468dd6e1ad86c28
Poojetha <dkpooji247@gmail.com>
2025-09-16 11:07:08 +0530

    method

--- demo.java
+++ demo.java
@@ -1,9 +1,9 @@
 public class demo {
-    int coffee = 20;
-    int tea = 10;
+    void greeting(){
+        System.out.println("welcome");
+    }
     public static void main(String args[]) {
     	demo d1 = new demo ();
-    	System.out.println(d1.coffee);
-    	System.out.println(d1.tea);
+    	d1.greeting();
     }
 }
