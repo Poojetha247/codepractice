9f9aa0eb6d93488f3749bd94489fa189853d9624
Poojetha <dkpooji247@gmail.com>
2025-09-16 15:28:04 +0530

    function

--- demo.java
+++ demo.java
@@ -3,11 +3,13 @@ public class demo {
     int coffee = 20;
     int tea = 10;
     int juice = 15;
+    void sum(){
+        int total = coffee +tea+juice;
+        System.out.println(total);
+    }
     public static void main(String args[]) {
        demo d1 = new demo();
-       System.out.println(d1.coffee);
-       System.out.println(d1.tea);
-       System.out.println(d1.juice);
+       d1.sum();
         }
     }
 
