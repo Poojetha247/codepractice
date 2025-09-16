bac5f8fc2055767749485c1c80d63a4247eaf360
Poojetha <dkpooji247@gmail.com>
2025-09-16 15:20:32 +0530

    class and object

--- demo.java
+++ demo.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class demo {
+    int coffee = 20;
+    int tea = 10;
+    int juice = 15;
     public static void main(String args[]) {
-       Scanner scan= new Scanner (System.in);
-       int end = scan.nextInt();
-       int i = 1;
-       while(i<=end) {
-    	   System.out.println(i);
-    	   i++;
-       }
+       demo d1 = new demo();
+       System.out.println(d1.coffee);
+       System.out.println(d1.tea);
+       System.out.println(d1.juice);
         }
     }
 
