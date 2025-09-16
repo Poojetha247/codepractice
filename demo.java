53a4daf24a615ba037d6500f98a15054ad768b65
Poojetha <dkpooji247@gmail.com>
2025-09-16 11:14:09 +0530

    user input and if else

--- demo.java
+++ demo.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 public class demo {
-    void greeting(){
-        System.out.println("welcome");
-    }
     public static void main(String args[]) {
-    	demo d1 = new demo ();
-    	d1.greeting();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num%3==0 && num %5 == 0){
+            System.out.println("FizzBuzz");
+
+        }  
+        else if (num %3==0){
+            System.out.println("Fizz");
+        }  	
+        else if (num % 5 ==0){
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println("Not divisible by 3 or 5");
+        }
     }
 }
