c183b9954ac46a2e1e153b5c9e79b0e53c85f186
Poojetha <dkpooji247@gmail.com>
2025-09-16 10:59:26 +0530

    class and object

--- demo.java
+++ demo.java
@@ -1,12 +1,9 @@
-
-import java.util.Random;
 public class demo {
-	int coffee = 20;
-	int tea = 10;
+    int coffee = 20;
+    int tea = 10;
     public static void main(String args[]) {
     	demo d1 = new demo ();
     	System.out.println(d1.coffee);
     	System.out.println(d1.tea);
-        
-    	}
+    }
 }
