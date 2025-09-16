52b09895dd15354a577755879f6afa43f5a5a6c4
Poojetha <dkpooji247@gmail.com>
2025-09-16 10:46:43 +0530

    random while

--- demo.java
+++ demo.java
@@ -3,9 +3,12 @@ import java.util.Random;
 public class demo {
     public static void main(String args[]) {
     	Random rand = new Random();
-		int num = rand.nextInt();
-		System.out.println(num);
+		int num = 0;
+		while(num!=5){
+			 num = rand.nextInt();
+		}
+
     	}
-		
+
 }
 
