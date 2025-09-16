0cb292f27cfcd8021f3a8da3898e8292d09dad74
Poojetha <dkpooji247@gmail.com>
2025-09-16 14:11:13 +0530

    while loop

--- demo.java
+++ demo.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 public class demo {
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        int size = scan.nextInt();
-        int num[]= new int [size];
-        for (int i = 0;i<size;i++){
-            num[i]= scan.nextInt();
+       Scanner scan= new Scanner (System.in);
+       int end = scan.nextInt();
+       int i = 1;
+       while(i<=end) {
+    	   System.out.println(i);
+    	   i++;
+       }
+        }
+    }
 
-        }
-        for (int j =0;j<size;j++){
-            System.out.println(num[j]);
-        }
-            
-}
-}
 
