d4a099b39691670de1aa5d2e80eef0bdbfc51609
Poojetha <dkpooji247@gmail.com>
2025-09-16 11:49:47 +0530

    for loop

--- demo.java
+++ demo.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 public class demo {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-       int Salary = scan.nextInt();
-       int age = scan.nextInt();  
-       if (Salary>=20000&& age <= 25? true : false){
-           System.out.println("eligible for loan");
-       }
-       else {
-        System.out.println("you are not eligible for loan");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+       for (int i = a; i<=b;i++){
+        System.out.println(i);
        }
     }
 }
