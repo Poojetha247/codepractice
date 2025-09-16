bf5b7bcbc743737e5c7635eaa3c715b3e2db6d9b
Poojetha <dkpooji247@gmail.com>
2025-09-16 12:21:55 +0530

    array

--- demo.java
+++ demo.java
@@ -2,22 +2,16 @@ import java.util.Scanner;
 public class demo {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int evencount = 0;
-        int oddcount = 0;
-       for (int i = a; i<=b;i++){
-        if (i%2==0){
-             
-            evencount++;
+        int size = scan.nextInt();
+        int num[]= new int [size];
+        for (int i = 0;i<size;i++){
+            num[i]= scan.nextInt();
+
+        }
+        for (int j =0;j<size;j++){
+            System.out.println(num[j]);
+        }
             
-        }
-        else {
-        	oddcount++;
-        }
-       }
-       System.out.println("the even count is :"+ evencount);
-       System.out.println("the odd count is :"+ oddcount);
-    }
+}
 }
 
