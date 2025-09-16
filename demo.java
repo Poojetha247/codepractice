d7c983defaa43ab1bcf27b820f6a0acec13321b8
Poojetha <dkpooji247@gmail.com>
2025-09-16 12:14:18 +0530

    for loop count

--- demo.java
+++ demo.java
@@ -4,8 +4,20 @@ public class demo {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
+        int evencount = 0;
+        int oddcount = 0;
        for (int i = a; i<=b;i++){
-        System.out.println(i);
+        if (i%2==0){
+             
+            evencount++;
+            
+        }
+        else {
+        	oddcount++;
+        }
        }
+       System.out.println("the even count is :"+ evencount);
+       System.out.println("the odd count is :"+ oddcount);
     }
 }
+
