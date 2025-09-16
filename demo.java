469bc713785e8b1b7d88fe4d49a61cfb2e585bc8
Poojetha <dkpooji247@gmail.com>
2025-09-16 11:35:11 +0530

    nested if

--- demo.java
+++ demo.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 public class demo {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        if (name.equals("mark")){
-            System.out.println("hello mark");
+       int Salary = scan.nextInt();
+       int age = scan.nextInt();  
+       if (Salary>=20000&& age <= 25){
+        int loan = scan.nextInt();
+        if (loan<=50000){
+            System.out.println("you are eligible for loan");
         }
-        else{
-            System.out.println("who are you?");
+        else {
+            System.out.println("maximum loan amount is 50000");
         }
+       }
+       else {
+        System.out.println("not eligible for loan");
+       }  
     }
 }
