90d75302312c9d11857cc1f54a0944a31f684ebc
Poojetha <dkpooji247@gmail.com>
2025-09-17 12:24:48 +0530

    enhanced for loop

--- demo.java
+++ demo.java
@@ -1,31 +1,14 @@
+
 import java.util.Scanner;
 public class demo {
-    int sum(int a,int b){
-        return a+b;
-    }
-    int sub(int a, int b){
-        return a-b;
-    }
-    int mul(int a , int b){
-        return a*b;
-    }
-    int div(int a ,int b){
-        return a/b;
-    }
-    void add (){
-        System.out.println("lorem ipsum");
-    }
     public static void main(String args[]) {
-       demo d1 = new demo();
-       int addition = d1.sum(34,76);
-       int subtraction = d1.sub(98,43);
-       int multiply = d1.mul(54,98);
-       int division = d1.div(45,3);
-       System.out.println(addition);
-       System.out.println(subtraction);
-       System.out.println(multiply);
-       System.out.println(division);
-       d1.add();
+    	Scanner scan = new Scanner(System.in);
+      int num[]=new int [5];
+      for (int i = 0 ;i<5;i++) {
+    	num[i]= scan.nextInt();      }
+      for (int var:num){
+        System.out.println(var);
+      }
         }
     }
 
