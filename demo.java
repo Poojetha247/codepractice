87681e326c70ebff8580b55e8e016ced919cadfd
Poojetha <dkpooji247@gmail.com>
2025-09-18 01:39:10 +0530

    final variable

--- demo.java
+++ demo.java
@@ -1,15 +1,12 @@
 public class Demo  {
-	static {
-		System.out.println("hello");
-	}
+	
 	
     public static void main(String args[]) {
-    	System.out.println("hello world");
-    	Demo d1 = new Demo();
-    	Demo d2 = new Demo();
+    	final int age = 19;
+    	System.out.println(age);
+    	
        }
     }
-
 
 
 
