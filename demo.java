19a99dd86aea169eb014ed864d0674a9ffc8f6c5
Poojetha <dkpooji247@gmail.com>
2025-09-17 13:59:40 +0530

    this keyword

--- demo.java
+++ demo.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class demo {
 	String name ;
 	int age;
-	demo(String a , int b ){
-		name = a ;
-		age= b;
+	demo(String name , int age ){
+		this.name = name ;
+		this.age= age;
 		System.out.println("heloo");
 	}
     public static void main(String args[]) {
     	demo d1 = new demo("john",23);
+    	demo d2 = new demo("henry",34);
     	System.out.println(d1.age);
+    	System.out.println(d2.name);
         }
     }
+
 
 
 
