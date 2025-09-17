5495abe24c85d3e5f974239d49923fea6ee01aff
Poojetha <dkpooji247@gmail.com>
2025-09-18 01:32:51 +0530

    static keyword

--- demo.java
+++ demo.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
  class Teacher{
 	static int totallesson ;
+    static String teachername;
  }
 class student extends Teacher {
 	void lesson() {
@@ -12,10 +13,11 @@ public class Demo  {
     public static void main(String args[]) {
     	
     	Teacher.totallesson = 5;
-    	
+        Teacher.teachername = "john";
     	Teacher.totallesson = 10;
     	System.out.println(Teacher.totallesson);
-       }
+        Teacher.teachername = "mark";
+        System.out.println(Teacher.teachername);       }
     }
 
 
