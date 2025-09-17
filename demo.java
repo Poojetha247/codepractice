637bb8dd711647d728db962708b8133c9a0f1c34
Poojetha <dkpooji247@gmail.com>
2025-09-17 14:30:49 +0530

    inheritance 1

--- demo.java
+++ demo.java
@@ -1,21 +1,54 @@
+
 import java.util.Scanner;
-public class demo {
-	String name ;
+class animal{
+	String name;
 	int age;
-	demo(String name , int age ){
-		this.name = name ;
-		this.age= age;
-		System.out.println("heloo");
+	void makesound() {
+		System.out.println("Animal makes sound");
 	}
+}
+class dog extends animal{
+	String  breed;
+	void makesound() {
+		System.out.println("dog barks");
+	}
+	void fetch() {
+		System.out .println("dog is fetching");
+	}
+}
+class cat extends animal{
+	String color;
+	void makesound() {
+		System.out.println("cat meows");
+	}
+	void climb() {
+		System.out.println("cat is climbing");
+	}
+}
+public class Demo {
+	
     public static void main(String args[]) {
-    	demo d1 = new demo("john",23);
-    	demo d2 = new demo("henry",34);
+    	dog d1 = new dog ();
+    	cat c1 = new cat ();
+    	d1.name = "jimmy";
+    	d1.age = 3;
+    	d1.breed="bull dog";
+    	d1.makesound();
+    	d1.fetch();
+    	c1.makesound();
+    	c1.climb();
+    	c1.name = "mile";
+    	c1.age = 4;
+    	c1.color ="black";
+    	System.out.println(d1.name);
     	System.out.println(d1.age);
-    	System.out.println(d2.name);
-        }
+    	System.out.println(d1.breed);
+    	System.out.println(c1.name);
+    	System.out.println(c1.age);
+    	System.out.println(c1.color);
+    	
+       }
     }
-
-
 
 
 
