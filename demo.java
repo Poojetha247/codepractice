26188d47973eb8593444a5e41b89bc29c4d9260c
Poojetha <dkpooji247@gmail.com>
2025-09-17 16:00:09 +0530

    super keyword

--- demo.java
+++ demo.java
@@ -1,39 +1,24 @@
-
 import java.util.Scanner;
-class animal{
-	
-	int age = 12;
-	animal(String name){
+class person{
+	String name;
+	person(String name){
+		this.name = name;
 		System.out.println(name);
 	}
-	void makesound() {
-		System.out.println("Animal makes sound");
+	
+}
+class employee extends person{
+	int employeid ;
+	employee(){
+		super("john");
 	}
 }
-class dog extends animal{
-	dog(){
-		super ("monkey");
-		super.makesound();
-		System.out.println("dog");
-		System.out.println(super.age);
-	}
-}
-class cat extends animal{
-	cat(){
-	super ("rabbit");
-	super.makesound();
-	System.out.println("cat");
-	System.out.println(super.age);
-	}
-}
+
 public class Demo {
 	
     public static void main(String args[]) {
-    	dog d1 = new dog ();
-    	cat c1 = new cat ();
+    	employee e1 = new employee();
     	
        }
     }
-
-
 
