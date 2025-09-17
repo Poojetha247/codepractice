e9295190bb546d3178e8fc00d7a023becfb9244a
Poojetha <dkpooji247@gmail.com>
2025-09-18 01:49:03 +0530

    final class

--- demo.java
+++ demo.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
-class a {
+final class a {
 	final void section() {
 		System.out.println("section a");
 	}
 }
-class b extends a{
-	
-}
- 
+
 public class Demo  {
 	
 	
     public static void main(String args[]) {
-    	b b1 = new b();
-    	b1.section();
-    	
+    	a a1 = new a();
+    	a1.section();
        }
     }
+
+
 
 
