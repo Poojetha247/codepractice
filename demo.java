4a6d9461f74f27b190f8b6fd57bb778f1c8a969a
Poojetha <dkpooji247@gmail.com>
2025-09-17 12:31:14 +0530

    constructor

--- demo.java
+++ demo.java
@@ -1,15 +1,16 @@
-
 import java.util.Scanner;
 public class demo {
+	int age;
+	demo(){
+		age= 12;
+		System.out.println("heloo");
+	}
     public static void main(String args[]) {
-    	Scanner scan = new Scanner(System.in);
-      int num[]=new int [5];
-      for (int i = 0 ;i<5;i++) {
-    	num[i]= scan.nextInt();      }
-      for (int var:num){
-        System.out.println(var);
-      }
+    	demo d1 = new demo();
+    	System.out.println(d1.age);
         }
     }
+
+
 
 
