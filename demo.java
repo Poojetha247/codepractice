f8e8ae8ca4a4da010f6f0ad45feb32a1358a5d86
Poojetha <dkpooji247@gmail.com>
2025-09-17 15:31:09 +0530

    inheritence

--- demo.java
+++ demo.java
@@ -1,51 +1,46 @@
 
-import java.util.Scanner;
-class animal{
-	String name;
-	int age;
-	void makesound() {
-		System.out.println("Animal makes sound");
+class vehicle{
+	String brand ;
+	int year;
+	void startengine(){
+		System.out.println("engine is stared");
 	}
 }
-class dog extends animal{
-	String  breed;
-	void makesound() {
-		System.out.println("dog barks");
+class car extends vehicle{
+	String feild_type ;
+	void startengine() {
+		System.out.println("car engine started");
 	}
-	void fetch() {
-		System.out .println("dog is fetching");
-	}
+    void drive() {
+    	System.out.println("car is driving");
+    }
 }
-class cat extends animal{
-	String color;
-	void makesound() {
-		System.out.println("cat meows");
-	}
-	void climb() {
-		System.out.println("cat is climbing");
-	}
+
+class truck extends vehicle{
+    int loadcapacity ;
+    void startenging() {
+    	System.out.println("truck engine started");
+    }
+    void haul() {
+    	System.out.println("truck is hauling");
+    }
 }
 public class Demo {
 	
     public static void main(String args[]) {
-    	dog d1 = new dog ();
-    	cat c1 = new cat ();
-    	d1.name = "jimmy";
-    	d1.age = 3;
-    	d1.breed="bull dog";
-    	d1.makesound();
-    	d1.fetch();
-    	c1.makesound();
-    	c1.climb();
-    	c1.name = "mile";
-    	c1.age = 4;
-    	c1.color ="black";
-    	System.out.println(d1.name);
-    	System.out.println(d1.age);
-    	System.out.println(d1.breed);
-    	System.out.println(c1.name);
-    	System.out.println(c1.age);
-    	System.out.println(c1.color);
+    	truck t1 = new truck ();
+    	car c1 = new car ();
+    	t1.startengine();
+    	t1.haul();
+    	c1.startengine();
+    	c1.drive();
+    	
+    	System.out.println(t1.brand= "werr");
+    	System.out.println(t1.year =23);
+    	System.out.println(t1.loadcapacity =345);
+    	System.out.println(c1.brand = "xyz");
+    	System.out.println(c1.year = 34);
+    	System.out.println(c1.feild_type ="fef");
     	
        }
     }
