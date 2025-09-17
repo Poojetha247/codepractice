1d2b1a6fa5b0205027cf82f4503e74b637e8b957
Poojetha <dkpooji247@gmail.com>
2025-09-17 13:51:59 +0530

    parameterized constructor

--- demo.java
+++ demo.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 public class demo {
+	String name ;
 	int age;
-	demo(){
-		age= 12;
+	demo(String a , int b ){
+		name = a ;
+		age= b;
 		System.out.println("heloo");
 	}
     public static void main(String args[]) {
-    	demo d1 = new demo();
+    	demo d1 = new demo("john",23);
     	System.out.println(d1.age);
         }
     }
