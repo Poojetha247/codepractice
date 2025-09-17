772a42c55e7f0a05da6dd7480e2faf36f6dad6ff
Poojetha <dkpooji247@gmail.com>
2025-09-18 01:26:08 +0530

    static keyword

--- demo.java
+++ demo.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
-class a{
-	protected int age = 19;
-	void age() {
-		System.out.println(age);
+ class Teacher{
+	static int totallesson ;
+ }
+class student extends Teacher {
+	void lesson() {
+		System.out.println("2 lesson completed");
 	}
 }
-
-public class Demo {
-	public int age = 19;
+public class Demo  {
+	
     public static void main(String args[]) {
-    	Demo d1 = new Demo();
-    	System.out.println(d1.age);
+    	student s1 = new student();
+    	s1.totallesson = 5;
+    	student s2 = new student ();
+    	s2.totallesson = 10;
+    	System.out.println(s1.totallesson);
        }
     }
 
