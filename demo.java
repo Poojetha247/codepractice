b43c85ff94a327b416680d383dc77dcc45e70b18
Poojetha <dkpooji247@gmail.com>
2025-09-23 22:16:01 +0530

    prime number

--- demo.java
+++ demo.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
-public class Demo{
-public static void main(String [] args){	
-Scanner scan = new Scanner(System.in);	
-int n = scan.nextInt();
-String[] res = {"even", "odd"};
-System.out.println(res[n % 2]);
+class Demo{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num =scan.nextInt();
+        if (num<=1){
+            System.out.println("Not a prime ");
+            return;
+        }
+        int c=2;
+        
+        while(c*c<=num){
+            if (num%c==0){
+                System.out.println("Not a prime number");
+               
+                break;
+            }
+            c++;
+            System.out.println("Prime number");
+        }
+        
+        
+    }
 }
-}
-
-
