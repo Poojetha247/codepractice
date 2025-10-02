50bb700088f6e319056bdf2dd401d797e34b86f3
Poojetha <dkpooji247@gmail.com>
2025-10-02 23:54:58 +0530

    countnumbers

--- demo.java
+++ demo.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 class Demo{
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int n = scan.nextInt();
-        int a = 0;
-        int b = 1;
-        int count = 2;
-        while (count<=n) {
-        	int temp = b;
-        	b = b+a;
-        	a= temp;
-        	count++;
-        	
-        }
-        System.out.println(b);
+      Scanner scan = new Scanner (System.in);
+      int num = scan.nextInt();
+      int count =0;
+      while (num>0) {
+    	  int rem = num%10;
+    	  if (rem == 5) {
+    		  count++;
+    	  }
+    	  num = num/10;
+      }
+      
+      System.out.println(count);
          }
 }
