e65a2808be33b6b8f57f8a68aa9f7588ef605b11
Poojetha <dkpooji247@gmail.com>
2025-10-04 00:58:34 +0530

    calculator

--- demo.java
+++ demo.java
@@ -2,14 +2,42 @@ import java.util.Scanner;
 class Demo{
     public static void main(String[] args) {
       Scanner scan = new Scanner (System.in);
-      int num = scan.nextInt();
-      int ans = 0;
-      while (num>0) {
-    	  int rem = num%10;
-    	  ans = ans *10+rem;
-    	  num = num/10;
+      int ans =0;
+      while(true) {
+    	  System.out.println("Enter the operator:");
+    	  char op = scan.next().trim().charAt(0);
+    	  if (op=='+'||op=='-'||op=='/'||op=='*'||op=='%') {
+    		  System.out.print("enter num1:");
+    		  int num1 = scan.nextInt();
+    		  System.out.print("enter num2:");
+    		  int num2 = scan.nextInt();
+    		  if (op=='+') {
+    			  ans=num1+num2;
+    		  }
+    		  if(op=='-') {
+    			  ans=num1-num2;
+    		  }
+    		  if(op=='*') {
+    			  ans=num1*num2;
+    		  }
+    		  if(op=='/') {
+    			  if(num2!=0) {
+    			  ans=num1/num2;
+    			  }
+    		  }
+    		  if(op=='%') {
+    			  ans=num1%num2;
+    		  }
+    		  System.out.println(ans);
+    	  }
+    	  else if(op=='x'||op=='X') {
+    		  System.out.println("Existing calculator");
+    		  break;
+    	  }
+    	  else {
+    		  System.out.println("invalid operator");
+    	  }
       }
-      
-      System.out.println(ans);
+     
          }
 }
