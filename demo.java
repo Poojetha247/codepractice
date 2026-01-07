f906aa3376512889ad4df1bc7cc1a4fee7374b5a
Poojetha <dkpooji247@gmail.com>
2026-01-07 21:00:22 +0530

    calculator

--- demo.java
+++ demo.java
@@ -12,23 +12,7 @@ public class Demo {
 				System.out.println("Enter the two numbers:");
 				int num1 = scan.nextInt();
 				int num2 = scan.nextInt();
-				if(op == '+') {
-					ans = num1 +num2;
-				}
-				if(op == '-') {
-					ans = num1-num2;
-				}
-				if(op =='*') {
-					ans = num1*num2;
-				}
-				if(op == '/') {
-					if(num2>0) {
-						ans = num1/num2;
-					}
-				}
-				if(op == '%') {
-					ans = num1%num2;
-				}
+				
 				
 			}
 			else if(op == 'x'|| op == 'X') {
