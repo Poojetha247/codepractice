9311ceeb73a757d62595e240a1dadb0c159fe27b
Poojetha <dkpooji247@gmail.com>
2026-03-10 12:25:15 +0530

    ArrayList add remove set

--- demo.java
+++ demo.java
@@ -2,9 +2,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Demo{
 	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
 		ArrayList<Integer>list = new ArrayList <>(10);
-	    list.add(234);
-	    list.add(56);
-		System.out.println(list);
+	    for(int i= 0; i<10;i++){
+            list.add(scan.nextInt());
+		}
+		for (int i = 0;i<10;i++){
+	         System.out.println(list.get(i));
+        }	
+		list.add (100);
+		list.set(1,34);
+		list.remove(4);
+		System.out.println(list);	
 	}
 }
+ 
