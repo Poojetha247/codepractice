ab9084e39897ba90f15c9f9a58518671a0586d9b
Poojetha <dkpooji247@gmail.com>
2026-03-10 22:27:20 +0530

    find the maximum in array

--- demo.java
+++ demo.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 public class Demo{
 	public static void main(String[]args){
 	int[] arr = {1,2,3,4,5};
-	swap(arr,2,0);
-	System.out.println(Arrays.toString(arr));
+	
+	System.out.println(max(arr););
 	}
-	static void swap(int [] arr,int index1, int index2){
-		int temp = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] =temp;
-		
+	static int max(int [] arr){
+		int max =0;
+		for(int i= 0;i< arr.length;i++){
+			if(max<arr[i]){
+				max= arr[i];
+			}
+		}
+		return max;
 	}
 }
  
