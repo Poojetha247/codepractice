7727b9a988f9b5c59da07772413de2ec5aef38e2
Poojetha <dkpooji247@gmail.com>
2026-03-23 15:41:16 +0530

    find max in array

--- demo.java
+++ demo.java
@@ -3,14 +3,13 @@ import java.util.*;
 import java.util.Scanner;
 public class Demo{
 	public static void main(String[]args){
-	int [] num = {2,4,3,56,78,546};
-	int count =0;
-	for(int x: num){
-		if(x%2==0){
-           count++;
-		}
+	int[] num = {2,4,3,56,78,546};
+	int max =Integer.MIN_VALUE;
+	for(int x :num){
+		 max = Math.max(max,x);
 	}
-	System.out.println(count);
+     System.out.println(max);
+	
 	}
 }
  
