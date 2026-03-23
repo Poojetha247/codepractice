273cfce037e20534d511dafcf59e9f0996a88ffa
Poojetha <dkpooji247@gmail.com>
2026-03-23 12:15:26 +0530

    number of vowels in a string

--- demo.java
+++ demo.java
@@ -3,24 +3,14 @@ import java.util.*;
 import java.util.Scanner;
 public class Demo{
 	public static void main(String[]args){
-	int [] arr = {34,67,22,54,97,12,45};
-	int target = 54;
-	int start = 2;
-	int end = 6;
-	int ans = linearSearch(arr , target,start,end);
-	System.out.println(ans);
+	String name = "poojetha";
+	int count = 0;
+	for(char c : name.toCharArray()){
+		if("aeiou".indexOf(c) != -1){
+			count++;
+		}
 	}
-	static int linearSearch(int [] arr , int target, int start,int end){
-		if(arr.length == 0){
-			return -1;
-		}
-		for (int i = start; i<end;i++)	{
-			if (target == arr[i]){
-				return i;
-			}
-		}
-	return -1;
-        
+	System.out.println(count);
 	}
 }
  
