d200c43f1f0edfa4bd7104a4879c42b103faf20d
Poojetha <dkpooji247@gmail.com>
2026-03-23 15:58:06 +0530

    product of array element

--- demo.java
+++ demo.java
@@ -3,9 +3,12 @@ import java.util.*;
 import java.util.Scanner;
 public class Demo{
 	public static void main(String[]args){
-	String s="Hello to infosys";
-	String[] part = s.trim().split(" +");
-	System.out.println(part.length);
+	int [] num  = {1,2,3};
+	int product = 1;
+	for(int x: num) {
+		product *= x;
+	}
+	System.out.println(product);
 	}
 }
  
